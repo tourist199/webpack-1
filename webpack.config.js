caf9d555ddb1227a2553356c3d658652d433db02
Khanh <ltk1909@gmail.com>
2020-02-20 10:54:31 +0700

    .

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+const config = {
+    entry : './src/index.js',
+    output: {
+        filename : 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    }
+}
+module.exports = config;
